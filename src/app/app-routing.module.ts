6c1c7d7211e3cd7b321c5233b0f8a459a944a19a
Artur Kuznetsov <artkuznetsov191@gmail.com>
2019-03-21 09:46:51 +0200

    master/feat(home): add styles, layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'login', component: LoginComponent },
+  // { path: 'books', component: BooksPageComponent, canActivate: [AuthGuard]},
+  // { path: 'detail/:id', component: BooksDetailsComponent, canActivate: [AuthGuard] },
+  // { path: 'add-book', component: AddBookComponent, canActivate: [AuthGuard] },
+  // { path: 'edit-book/:id', component: EditBookComponent, canActivate: [AuthGuard] },
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './ui/header/header.component';
+import { FooterComponent } from './ui/footer/footer.component';
+import { SliderComponent } from './ui/slider/slider.component';
+import { InlineSVGModule } from 'ng-inline-svg';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SliderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    InlineSVGModule.forRoot(),
+    CarouselModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
