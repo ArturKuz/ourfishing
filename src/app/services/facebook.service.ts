5dddd43d312065b843cc5c065aefcc481ddf7d5a
PZapleshnikov <zap20@tut.by>
2019-11-18 13:16:34 +0300

    added facebook login options

--- src/app/services/facebook.service.ts
+++ src/app/services/facebook.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
-import { FacebookService, InitParams, LoginResponse } from 'ngx-facebook';
+import { FacebookService, InitParams, LoginResponse, LoginOptions } from 'ngx-facebook';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacebookApiService {
-
+  options: LoginOptions = {
+    scope: 'email,public_profile'
+  };
   constructor(private fb: FacebookService) {
 
     const initParams: InitParams = {
       appId: '432523634223755',
       xfbml: true,
-      version: 'v2.8'
+      version: 'v2.11'
     };
 
     fb.init(initParams);
@@ -20,10 +22,8 @@ export class FacebookApiService {
   }
 
   loginWithFacebook(): void {
-
-    this.fb.login()
+    this.fb.login(this.options)
       .then((response: LoginResponse) => console.log(response))
       .catch((error: any) => console.error(error));
-
   }
 }
